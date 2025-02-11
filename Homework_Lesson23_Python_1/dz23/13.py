8f111877216847f8450f95cd2c06f4f8c943b166
sttewiee <plapikovv@gmail.com>
2025-02-11 12:31:07 +0300

    -

---
+++ Homework_Lesson23_Python_1/dz23/13.py
@@ -0,0 +1,4 @@
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+average = (num1 + num2) / 2
+print(f"Среднее арифметическое: {average}")
---
+++ Homework_Lesson23_Python_1/dz23/9.py
@@ -0,0 +1,4 @@
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+product = num1 * num2
+print(f"Произведение чисел: {product}")
